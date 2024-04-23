b973d2a7832ad4b81a7586472c8567762e94aee8
Davi Russi <davirussi@gmail.com>
2024-04-22 22:39:37 -0300

    modelo de dados
    modelo de dados utilizados pelo sicar

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import geopandas as gpd
 
-# Load the shapefile
+# CArregar o shapefile
 gdf = gpd.read_file('palmeira/municipio_palmeira.shp')
+
+#formato utilizado nos dados do sicar
+
+#cod_tema;nom_tema;cod_imovel;mod_fiscal;num_area;ind_status;ind_tipo;des_condic;municipio;cod_estado
+#AREA_IMOVEL;Area do Imovel;RS-4313706-ACCCBEB81F2F48348595909C8523B84D;0,1927;3,0835;AT;IRU;Aguardando analise;Palmeira das Missoes;
+
+
 
 # Iterate over each row and save individual shapefiles
 for index, row in gdf.iterrows():
