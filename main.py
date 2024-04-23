715a9d1727cd1e7838a1be229725081fa22b9a1c
Davi Russi <davirussi@gmail.com>
2024-04-22 22:55:12 -0300

    Update main.py
    criando os diretórios de outpput

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import geopandas as gpd
+from pathlib import Path
 
 # CArregar o shapefile
+
+# Carregar o shapefile
 gdf = gpd.read_file('palmeira/municipio_palmeira.shp')
 
 #formato utilizado nos dados do sicar
 
 #cod_tema;nom_tema;cod_imovel;mod_fiscal;num_area;ind_status;ind_tipo;des_condic;municipio;cod_estado
 #AREA_IMOVEL;Area do Imovel;RS-4313706-ACCCBEB81F2F48348595909C8523B84D;0,1927;3,0835;AT;IRU;Aguardando analise;Palmeira das Missoes;
+
+#cidades que irá salvar, verificar se a pasta já existe senão criar
+cidades = ['Palmeira das Missoes','Boa Vista das Missoes', 'Jaboticaba']
+
+for cidade in cidades:
+    path = Path('output/'+cidade)
+    path.mkdir(parents=True, exist_ok=True)
+
 
 
 
@@ -19,5 +30,5 @@ for index, row in gdf.iterrows():
 
     # Filter the GeoDataFrame to get only the current row and save it
     single_item_gdf = gdf[gdf['cod_imovel'] == cod_imovel]
-    output_filename = f"output/{cod_imovel}.shp"
+    output_filename = f"output/{municipio}/{cod_imovel}.shp"
     single_item_gdf.to_file(output_filename)
