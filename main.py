f69701e0c11d22b00c152962a18272d15cf67ae1
Davi Russi <davirussi@gmail.com>
2024-04-22 21:37:24 -0300

    novo metodo
    novo método para iterar entre os cars do shapefile

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-def main():
-  print ('teste');
-  
-main()
+import geopandas as gpd
+
+# Load the shapefile
+gdf = gpd.read_file('palmeira/municipio_palmeira.shp')
+
+# Iterate over each row and save individual shapefiles
+for index, row in gdf.iterrows():
+    # Extract the 'cod_imovel' property
+    cod_imovel = row['cod_imovel']
+
+    # Filter the GeoDataFrame to get only the current row and save it
+    single_item_gdf = gdf[gdf['cod_imovel'] == cod_imovel]
+    output_filename = f"output/{cod_imovel}.shp"
+    single_item_gdf.to_file(output_filename)
